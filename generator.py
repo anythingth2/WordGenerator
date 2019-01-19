2ca8cd026a5cef3d65b0591dd22c7eada464a012
ChiChaChai <chatchaishaetan@gmail.com>
2019-01-19 22:11:53 +0700

    Add concate image

--- generator.py
+++ generator.py
@@ -1,11 +1,16 @@
+# -*- coding: utf8 -*-
+
 import cv2
 import numpy as np
 import os
 import matplotlib.pyplot as plt
+import random
+from PIL import Image
 
 
 class CharacterDataset:
-    def __init__(self, label, imgs):
+
+    def __init__(self, label,  imgs):
         self.label = label
         self.imgs = imgs
 
@@ -14,16 +19,55 @@ class CharacterDataset:
             raise Exception('wrong load path')
 
         label = os.path.basename(path)
-        imgs = [cv2.imread(os.path.join(path, imgPath))
+
+        imgs = [np.asarray(Image.open(os.path.join(path, imgPath)))
                 for imgPath in os.listdir(path)]
         return CharacterDataset(label, imgs)
 
+    def getRandomImage(self):
+        return self.imgs[random.randint(0, len(self.imgs)-1)]
+
+
+class Word:
+
+    def __init__(self):
+        self.img = None
+
+    def push(self, charDataset):
+        charImg = charDataset.getRandomImage()
+        if self.img is None:
+            self.img = charImg
+            return self.img
+
+        self.img = np.concatenate((self.img, charImg), axis=1)
+        return self.img
+
 
 class Generator:
-    def __init__(self):
-        self.charDatasets = []
+    def __init__(self, charaterDatasetPath):
+        self.characterDatasetPath = charaterDatasetPath
+        self.charDatasets = [CharacterDataset.loadFrom(os.path.join(
+            self.characterDatasetPath, charPath)) for charPath in os.listdir(self.characterDatasetPath)]
+
+    def __getDatasetByChar(self, char):
+        for charDataset in self.charDatasets:
+            if charDataset.label == char:
+                return charDataset
+        return None
+
+    def generate(self, word):
+        chars = list(word)
+        word = Word()
+        for char in chars:
+            charDataset = self.__getDatasetByChar(char)
+            word.push(charDataset)
+            self.imshow(word.img)
+
+    def imshow(self, img):
+        cv2.imshow('generator', img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
 
-generator = Generator()
-charDataset = CharacterDataset.loadFrom('characters/a')
-
+generator = Generator('characters')
+generator.generate('กกกก')
