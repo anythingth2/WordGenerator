7ea97824cb1064aa2f2fa8b767e49c661227cb17
ChiChaChai <chatchaishaetan@gmail.com>
2019-01-25 22:40:33 +0700

    Add Fix-size output from character generator

--- characterGenerator.py
+++ characterGenerator.py
@@ -15,11 +15,47 @@ class Character:
 
 
 class ImagePackage:
+    SIZE = 32
+
     def __init__(self, image, annotation):
         self.image = image
         self.annotation = annotation
         self.characters = []
         self.createCharacter()
+
+    def __preprocess(self, img):
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img = cv2.medianBlur(img, 5)
+        # img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+        _, img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+        _, contours, hierarchy = cv2.findContours(img, cv2.RETR_TREE, 3)
+        contours.sort(key=lambda cnt: cv2.contourArea(cnt, True), reverse=True)
+        cnt = contours[0]
+        x, y, w, h = cv2.boundingRect(cnt)
+        # cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),1)
+        img = img[y:y+h, x:x+w]
+
+        if w < h:
+            factor = ImagePackage.SIZE/h
+        else:
+            factor = ImagePackage.SIZE / w
+        img = cv2.resize(img, None, fx=factor, fy=factor,)
+        h, w = img.shape
+        top, bottom, left, right = 0, 0, 0, 0
+        if w < h:
+            left = (ImagePackage.SIZE-w)//2
+            right = left
+        else:
+            top = (ImagePackage.SIZE-h)//2
+            bottom = top
+        if left+right+w<ImagePackage.SIZE:
+            left +=1
+        if top+bottom+h<ImagePackage.SIZE:
+            top +=1
+        img = cv2.copyMakeBorder(
+            img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=255)
+
+        return img
 
     def createCharacter(self,):
         objs = self.annotation['objects']
@@ -31,7 +67,7 @@ class ImagePackage:
             left, top, right, bottom = int(left), int(
                 top), int(right), int(bottom)
             cropImage = self.image[top:bottom, left:right]
-
+            cropImage = self.__preprocess(cropImage)
             tags = obj['tags']
             if len(tags) > 0:
                 tag = tags[0]
@@ -75,7 +111,8 @@ class SuperviselyDecoder:
                     if not os.path.exists(savedFolder):
                         os.mkdir(savedFolder)
                     i += 1
-                    Image.fromarray(character.image).save('{}/{}.png'.format(savedFolder, i))
+                    Image.fromarray(character.image).save(
+                        '{}/{}.png'.format(savedFolder, i))
 
 
 decoder = SuperviselyDecoder(
